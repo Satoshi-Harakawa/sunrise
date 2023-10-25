3151a8d8b23647d9fff90080f261147c787c2b66
Satoshi-Harakawa <hrkw0902sts@outlook.jp>
2023-10-25 12:42:35 +0900

    [add]投稿編集機能、投稿削除機能

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 use Cloudinary;
 
@@ -29,5 +30,21 @@ class PostController extends Controller
     public function show(Post $post){
         $api_key = config('app.api_key');
         return view('posts/show')->with(['post' => $post, 'api_key' => $api_key]);
+    }
+    
+    public function edit(Post $post){
+        return view('posts/edit')->with(['post' => $post]);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function delete(Post $post){
+        $post->delete();
+        return redirect('/');
     }
 }
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -9,38 +9,39 @@
     
     <body class="antialiased">
         <h1>投稿</h1>
-        <form action="/posts" method="POST" enctype="multipart/form-data">
+        <form action="/posts/{{ $post->id }}" method="POST" enctype="multipart/form-data">
             @csrf
+            @method('PUT')
             <div class="title">
                 <h2>地名</h2>
-                <input type="text" name="post[title]" placeholder="タイトル" value="{{old('post.title')}}" />
+                <input type="text" name="post[title]"  value="{{ $post->title }}" />
             </div>
             
             <div class="Body">
                 <h2>本文</h2> 
-                <textarea name="post[body]" placeholder="今日も１日お疲れ様でした。">{{old('post.body')}}</textarea>
+                <textarea name="post[body]" value="{{ $post->body }}"></textarea>
             </div>
             
             <div class="Place">
                 <h2>都道府県</h2>
-                <input type="text" id="prefecture" name="post[prefecture]" placeholder=",〇〇県など" value="" />
+                <input type="text" id="prefecture" name="post[prefecture]" value="{{ $post->prefecture }}" />
                
                 <h2>市区町村</h2>
-                <input type="text" id="city" name="post[city]" placeholder="〇〇市〇〇町など" value="" />
+                <input type="text" id="city" name="post[city]" value="{{ $post->city }}" />
                
                 <h2>それ以下の住所</h2>
-                <input type="text" id="after_address" name="post[after_address]" placeholder="〇-〇-〇など" value="" />
+                <input type="text" id="after_address" name="post[after_address]" value="{{ $post->after_address }}" />
             </div>
             
             <div class="image">
                 <input type="file" name="image">
             </div>
             
-            <input type="submit" value="store"/>
+            <input type="submit" value="保存"/>
         </form>
         
         <div class="footer">
-            <a href="/">戻る</a>
+            <a href="/posts/{{ $post->id }}">戻る</a>
         </div>
     </body>
 </html>
